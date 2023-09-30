cb93756274efcb122854c884af18bfa8cac1eaf9
tianer1123 <q2719833@126.com>
2023-09-30 15:44:43 +0800

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -23,6 +23,17 @@ return {
 
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "day" },
-    -- opts = { style = "night" },
+    -- opts = { style = "day" },
+    -- opts = { style = "storm" },
+    opts = {
+        style = "night",
+        terminal_colors = true,
+        styles = {
+            comments = { italic = true },
+            keyworkds = { italic = true, bold = true },
+            functions = { bold = true },
+            variables = { bold = true },
+        }
+    },
+    -- opts = { style = "moon" },
 }
