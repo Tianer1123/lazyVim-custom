c29515c8b518c5c7f5040124d50ab38a0fa98070
tianer1123 <q2719833@126.com>
2023-10-13 16:13:23 +0800

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -27,12 +27,14 @@ return {
     -- opts = { style = "storm" },
     opts = {
         style = "night",
+        -- style = "storm",
         terminal_colors = true,
         styles = {
             comments = { italic = true },
-            keyworkds = { italic = true, bold = true },
-            functions = { bold = true },
-            variables = { bold = true },
+            keyworkds = { italic = true },
+            -- keyworkds = { italic = true, bold = true },
+            -- functions = { bold = true },
+            -- variables = { bold = true },
         }
     },
     -- opts = { style = "moon" },
